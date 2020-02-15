22208195a74db0f7d29d97db72c558971184aaff
Hiroki <face9363>
2020-02-15 13:23:09 +0900

    set some tag

--- apps/models/spot.py
+++ apps/models/spot.py
@@ -1,7 +1,19 @@
 class Spot:
-    
-    def __init__(self, lat, lng, name, review=0.0):
+
+    SUPERMARKET = 1 # スーパー
+    CONVINIENCESTOROE = 2 # コンビニ
+    NOODLE = 3 # ラーメン
+    SENTO = 4 # 銭湯
+    BAKERY = 5 # パン屋
+    KYOTOUNIV = 6 # 京大
+    DAIMONJI = 7 # 大文字
+    DRAGSTORE = 8 # ドラックストア
+    BANK = 9 # 銀行
+    CARSCHOOL = 10 # 車高
+
+    def __init__(self, lat: float, lng: float, name: str, tag: int, review=0.0):
         self.lat = lat
         self.lng = lng
         self.name = name
+        self.tag = tag
         self.review = review
