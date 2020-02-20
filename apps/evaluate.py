e51a3c2e3172566969e68a9edbe822d02c1da839
Hiroki <face9363>
2020-02-20 16:48:14 +0900

    all places

--- apps/evaluate.py
+++ apps/evaluate.py
@@ -1,12 +1,23 @@
 from apps import wave, mongo
 from apps.assess import PolynomialSpotFactor
+from apps.models import Spot
 
 def getData(tag, factor):
     collections = mongo.db.PLACE.find({"tag": tag})
     for spot in collections:
         factor.appendData(spot["name"], spot["lat"], spot["lng"], float(spot["rating"])/3)
 
-superFactor = PolynomialSpotFactor("スーパー")
-getData(1, superFactor) 
-wave.appendFactor(superFactor, tag=1, weight=1.2)
+for i in [
+    ["スーパー", Spot.SUPERMARKET],
+    ["コンビニ", Spot.CONVINIENCESTOROE],
+    ["ラーメン", Spot.NOODLE],
+    ["銭湯", Spot.SENTO],
+    ["パン屋", Spot.BAKERY],
+    ["薬局", Spot.DRAGSTORE],
+    ["銀行", Spot.BANK],
+    ["車高", Spot.CARSCHOOL]
+]:
+    factor = PolynomialSpotFactor(i[0])
+    getData(i[1], factor)
+    wave.appendFactor(factor, tag=i[1], weight=1.2)
 
